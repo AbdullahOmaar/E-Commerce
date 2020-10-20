d1f55a19f8b6aa07cf35ef3da645ff3b2870cd83
AbdullahOmaar <abdullahomarrefaat@gmail.com>
2020-10-20 06:48:48 +0200

    third

---
+++ src/app/interface/category.ts
@@ -0,0 +1,5 @@
+export interface Category{
+  name?: string;
+  discription?: string
+  image?: any;
+}
---
+++ src/app/services/category.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireStorage} from '@angular/fire/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private fs: AngularFirestore, private storage: AngularFireStorage) { }
+
+  AddCategory(name: string,discription, image: any, ){
+    return new Promise((resolve, reject) => {
+      let ref = this.storage.ref('category/' + image.name)
+      ref.put(image).then(() =>{
+        ref.getDownloadURL().subscribe(photoUrl => {
+          console.log(photoUrl)
+          this.fs.collection(`category/` ).add({
+            name,
+            discription,
+            photoUrl,
+          }).then(() => resolve('add sucss'))
+        })
+      })
+    })
+  }
+
+  gitAllCategory() {
+    return this.fs.collection(`category/`).snapshotChanges()
+  }
+
+}
