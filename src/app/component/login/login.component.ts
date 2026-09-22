83dff6a3b679aa3a303aedc9244b4a164637e330
abdullah-omar1 <the.president.abdo@gmail.com>
2026-09-23 02:45:49 +0300

    fix: handle sign-in and partial signup failures with clear feedback

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,32 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../../services/auth.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent implements OnInit {
-
-  errMessage: string = ''
-  constructor(private as: AuthService,
-              private route:ActivatedRoute,
-              private router:Router) { }
-
-  ngOnInit(): void {
+@Component({ selector: 'app-login', templateUrl: './login.component.html', styleUrls: ['./login.component.scss'] })
+export class LoginComponent {
+  errMessage = '';
+  submitting = false;
+  constructor(private auth: AuthService, private route: ActivatedRoute, private router: Router) {}
+  async login(form: NgForm): Promise<void> {
+    if (form.invalid || this.submitting) { return; }
+    this.errMessage = '';
+    this.submitting = true;
+    try {
+      await this.auth.login(form.value.email, form.value.password);
+      const target = this.route.snapshot.queryParamMap.get('returnUrl');
+      await this.router.navigateByUrl(target && target.startsWith('/') && !target.startsWith('//') ? target : '/');
+    } catch { this.errMessage = 'Unable to sign in. Check your email and password and try again.'; }
+    finally { this.submitting = false; }
   }
-
-  login(form){
-    this.errMessage = ''
-    let data = form.value
-    this.as.login(data.email, data.password)
-      .then(res =>
-        this.router.navigate(['/']))
-
-      .catch(err =>
-        this.errMessage = err.message
-      )
-  }
-
 }
--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,35 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import {User} from '../../interface/user';
-import {AuthService} from '../../services/auth.service';
-import {UserService} from '../../services/user.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { User } from '../../interface/user';
 
-@Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
-})
-export class SignupComponent implements OnInit {
-  errMessage: string = ''
-  constructor(private as: AuthService,
-              private us: UserService,
-              private route:ActivatedRoute,
-              private router:Router) { }
-
-  ngOnInit(): void {
+@Component({ selector: 'app-signup', templateUrl: './signup.component.html', styleUrls: ['./signup.component.scss'] })
+export class SignupComponent {
+  errMessage = '';
+  submitting = false;
+  constructor(private auth: AuthService, private users: UserService, private router: Router) {}
+  async signup(form: NgForm): Promise<void> {
+    if (form.invalid || this.submitting) { return; }
+    this.errMessage = '';
+    this.submitting = true;
+    const data: User = form.value;
+    let created = false;
+    try {
+      const result = await this.auth.signup(data.email, data.password);
+      created = true;
+      await this.users.addNewUser(result.user.uid, data.name, data.phone);
+      await this.router.navigate(['/']);
+    } catch {
+      this.errMessage = created
+        ? 'Your account was created, but your profile could not be saved. Sign in with this email; do not register again.'
+        : 'Unable to create an account. Check your details or sign in if you already have an account.';
+    } finally { this.submitting = false; }
   }
-
-
-  signup(form){
-    let data: User= form.value
-    this.as.signup(data.email, data.password).then(res => {
-      this.errMessage = ''
-      this.us.addNewUser(res.user.uid, data.name, data.phone).then(()=>{
-        this.router.navigate(['/'])
-      }).catch(err => console.log('dd',err))
-      })
-      .catch(err => {
-              this.errMessage = err.message + 'ss'
-            })
-      }
 }
